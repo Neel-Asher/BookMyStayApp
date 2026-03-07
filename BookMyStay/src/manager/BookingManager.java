b2122cb437cf96fc3c46893b4bb7803d073507d7
Neel-Asher <neel.asher20@gmail.com>
2026-03-07 18:51:44 +0530

    Coded to implement Booking Request functionality for UC3

---
+++ BookMyStay/src/manager/BookingManager.java
@@ -0,0 +1,48 @@
+package manager;
+
+import java.util.Scanner;
+import model.Reservation;
+import service.BookingQueueService;
+
+/**
+ * Handles user interaction for booking requests.
+ * Acts as a controller between the UI
+ * and the booking queue service.
+ *
+ * @author Neel
+ * @version 1.0
+ */
+public class BookingManager {
+
+    private BookingQueueService bookingQueueService;
+
+    public BookingManager(BookingQueueService bookingQueueService) {
+        this.bookingQueueService = bookingQueueService;
+    }
+
+    public void createBookingRequest(Scanner scanner) {
+
+        System.out.print("Enter Guest Name: ");
+        String guestName = scanner.nextLine();
+
+        System.out.print("Enter Room Type: ");
+        String roomType = scanner.nextLine();
+
+        System.out.print("Enter Number of Nights: ");
+        int nights = scanner.nextInt();
+        scanner.nextLine();
+
+        Reservation reservation =
+                new Reservation(guestName, roomType, nights);
+
+        bookingQueueService.addBookingRequest(reservation);
+    }
+
+    public void processNextRequest() {
+        bookingQueueService.processNextBooking();
+    }
+
+    public void showQueue() {
+        bookingQueueService.viewQueue();
+    }
+}
